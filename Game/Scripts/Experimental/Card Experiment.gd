53c24d172882deaf8ee4b3390c3426fa80cc37d5
Sean Kuehl <teddscottfelt@gmail.com>
2021-08-03 16:54:26 -0400

    implemented 'basic idea of card' in a way that allows lots of color customization, next step is to solve the font size changing problem

--- Game/Scripts/Experimental/Card Experiment.gd
+++ Game/Scripts/Experimental/Card Experiment.gd
@@ -1,8 +1,40 @@
 extends KinematicBody2D
 
-
+#enemySprite.set_modulate(Color(59,2,2))
+#to undo color thing: enemySprite.set_modulate(Color(1,1,1,1))
+#I think last spot is alpha, could be used to get the solid change I want
 var mouseIsInTile = false
 var temp = false
+
+var cardName = ""
+var cardPicture = ""
+var cardDescription = ""
+
+var colorSubDict = {"RED": "res://Game/Assets/Images/Experimental/Red.png",
+"BLUE": "res://Game/Assets/Images/Experimental/Blue.png",
+"BLACK": "res://Game/Assets/Images/Experimental/Black.png"}
+
+onready var baseBackground = $BaseBackground/Background
+onready var colorBackground = $ColorBackground/sample
+onready var pictureBackground = $PictureBackground/Background
+onready var picture = $Picture/secondSample
+onready var descriptionOrEffectBackground = $DescriptionOrEffectBackground/Background
+onready var nameLabel = $Name
+onready var descriptionOrEffectLabel = $DescriptionOrEffect
+
+var baseBackgroundColor = load(colorSubDict["BLACK"])
+var colorBackgroundColor = load(colorSubDict["BLUE"])
+var pictureBackgroundColor = load(colorSubDict["RED"])
+var descriptionOrEffectBackgroundColor = load(colorSubDict["BLACK"])
+
+#modulate doesn't work for all colors in all ways, so I'm going to do things a little different
+
+
+
+
+var cardOwner = ""	#this will eventually be "PlayerOne" or "PlayerTwo"
+var clickedAndDraggedOn = false
+
 
 func ReadLinesFromFile(fileName):
 	var file = File.new()
@@ -26,17 +58,30 @@ func _ready():
 	
 	
 	var content = ReadLinesFromFile("res://Game/Assets/Card Files/Experimental.txt")
-		
-	$name.text = content[0]
-	$description.text = content[2]
+	
+	cardName = content[0]	
+	nameLabel.text = cardName
+	
+	cardDescription = content[2]
+	descriptionOrEffectLabel.text = cardDescription
+	
 	#for x in content:
 		#print(x)
 	
 	#load the image
-	var image = load("res://Game/Assets/Images/Experimental/"+content[1])
+	cardPicture = content[1]
+	var image = load("res://Game/Assets/Images/Experimental/"+cardPicture)
 	
-	$Sprite2/secondSample.texture = image
+	picture.texture = image
 	#actual pic is texture property
+	#it's just highlighting, which may be why it doesn't work on black
+	
+	#set background colors
+	baseBackground.texture = baseBackgroundColor
+	colorBackground.texture = colorBackgroundColor
+	pictureBackground.texture = pictureBackgroundColor
+	descriptionOrEffectBackground.texture = descriptionOrEffectBackgroundColor
+
 	
 	
 	
@@ -45,9 +90,9 @@ func _ready():
 	
 func get_input():
 	if Input.is_action_pressed("CLICK") and mouseIsInTile:
-		temp = true
+		clickedAndDraggedOn = true
 	if Input.is_action_just_released("CLICK"):
-		temp = false
+		clickedAndDraggedOn = false
 		
 	
 	
@@ -62,8 +107,10 @@ func _physics_process(_delta):
 	
 	var newPos = get_global_mouse_position()
 	#position = position.move_toward(newPos, delta * moveSpeed)
-	if temp:
+	if clickedAndDraggedOn:
 		position = newPos
+	
+	
 	
 	get_input()
    
