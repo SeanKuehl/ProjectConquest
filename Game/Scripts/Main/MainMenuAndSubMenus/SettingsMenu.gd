e2e193d7126cf5f804a0d8d1e51df812ec438c69
SeanKuehl <teddscottfelt@gmail.com>
2021-10-01 12:30:14 -0400

    disabled music since it doesn't work when the project is exported for some reason

--- Game/Scripts/Main/MainMenuAndSubMenus/SettingsMenu.gd
+++ Game/Scripts/Main/MainMenuAndSubMenus/SettingsMenu.gd
@@ -10,15 +10,15 @@ func _ready():
 	Settings.SetPanelToTheme($Panel)
 	
 	
-	var returnedVals = MusicManager.GetCurrentMenuMusic()	#first is music, second is position in song
-	var song = returnedVals[0]
-	var songPos = returnedVals[1]
-	$MenuMusic.stream = song
-	$MenuMusic.play(songPos)
+#	var returnedVals = MusicManager.GetCurrentMenuMusic()	#first is music, second is position in song
+#	var song = returnedVals[0]
+#	var songPos = returnedVals[1]
+#	$MenuMusic.stream = song
+#	$MenuMusic.play(songPos)
 
 
 func _on_BackButton_pressed():
-	MusicManager.SetMenuMusicPlaybackPosition($MenuMusic.get_playback_position())
+	#MusicManager.SetMenuMusicPlaybackPosition($MenuMusic.get_playback_position())
 	get_tree().change_scene("res://Game/Scenes/Main/MainMenuAndSubMenus/MainMenu.tscn")
 
 
